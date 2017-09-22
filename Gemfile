12bae3fa8902b530981f67f0107216ffe87efa42
Maxime Boue <abriko@yahoo.fr>
2017-09-22 23:40:39 +0200

    Add homepage front

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jquery-rails'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
+gem 'devise-i18n'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  def set_locale
+    I18n.locale = :fr
+  end
+
 end
