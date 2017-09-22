63d423f7e94f0130ed91952a014fdd04c6e8de39
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-09-23 00:26:53 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :company do
+    resources :hirings, only: [:index, :update]
+  end
+
+  resources :student_skills, only: [:new, :create]
+  resources :hirings, only: [:index, :show, :update]
 end
