d6fb1abba5e835996abdf6fcc2dbade214f66b56
Maxime Boue <abriko@yahoo.fr>
2017-09-24 00:29:24 +0200

    Front for Company hirings

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :student_skills, foreign_key: :student_id, class_name: "Student_Skill"
   has_many :skills, through: :student_skills
   validates :role, presence: true
-  validates :company_name, presence: true, if: :company?
+  validates :company, presence: true, if: :company?
   validates :level, presence: true, if: :student?
   validates :first_name, presence: true, if: :student?
   validates :last_name, presence: true, if: :student?
