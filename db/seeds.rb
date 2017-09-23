42a32c261823c3f4561d944abe5055c9060cd0c2
jules maregiano <jules.maregiano@gmail.com>
2017-09-24 00:19:01 +0200

    seed avec necessaire pour skill

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+User.create(email: "jules.maregiano@gmail.com", first_name: 'Jules', last_name: 'Maregiano', role: 'student', password: '123soleil', level: '3eme', phone: '0123456789')
+
+
+urls = ["http://res.cloudinary.com/zanzibar/image/upload/v1506202395/danka-peter-148724_ywiman.jpg","http://res.cloudinary.com/zanzibar/image/upload/v1506202400/climate-kic-350836_lefhcp.jpg"]
 Skill.create(name: "TimideSociable")
+Skill.last.photo_urls = urls
 puts "Création de la Skill #{Skill.last.name}"
-Skill.create(name: "CerebralPhysique")
+urls = ["http://res.cloudinary.com/zanzibar/image/upload/v1506202395/danka-peter-148724_ywiman.jpg","http://res.cloudinary.com/zanzibar/image/upload/v1506202400/climate-kic-350836_lefhcp.jpg"]
+Skill.create(name: "OrganiseCreatif")
+Skill.last.photo_urls = urls
 puts "Création de la Skill #{Skill.last.name}"
+urls = ["http://res.cloudinary.com/zanzibar/image/upload/v1506202395/danka-peter-148724_ywiman.jpg","http://res.cloudinary.com/zanzibar/image/upload/v1506202400/climate-kic-350836_lefhcp.jpg"]
 Skill.create(name: "CommercialCulturel")
+Skill.last.photo_urls = urls
 puts "Création de la Skill #{Skill.last.name}"
+urls = ["http://res.cloudinary.com/zanzibar/image/upload/v1506202395/danka-peter-148724_ywiman.jpg","http://res.cloudinary.com/zanzibar/image/upload/v1506202400/climate-kic-350836_lefhcp.jpg"]
 Skill.create(name: "PrivePublic")
+Skill.last.photo_urls = urls
 puts "Création de la Skill #{Skill.last.name}"
+urls = ["http://res.cloudinary.com/zanzibar/image/upload/v1506202395/danka-peter-148724_ywiman.jpg","http://res.cloudinary.com/zanzibar/image/upload/v1506202400/climate-kic-350836_lefhcp.jpg"]
 Skill.create(name: "ManuelInformatique")
+Skill.last.photo_urls = urls
 puts "Création de la Skill #{Skill.last.name}"
