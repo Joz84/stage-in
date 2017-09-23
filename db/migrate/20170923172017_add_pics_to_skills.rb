e68e6ad1eb2020fbea0ad6cb4e0ff1707184037f
jules maregiano <jules.maregiano@gmail.com>
2017-09-23 22:37:35 +0200

    Correctifs avant merge

--- db/migrate/20170923172017_add_pics_to_skills.rb
+++
@@ -1,6 +0,0 @@
-class AddPicsToSkills < ActiveRecord::Migration[5.0]
-  def change
-    add_column :skills, :photo1, :string
-    add_column :skills, :photo2, :string
-  end
-end
--- db/migrate/20170923180140_add_photo_to_skills.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToSkills < ActiveRecord::Migration[5.0]
-  def change
-    add_column :skills, :photo, :string
-  end
-end
