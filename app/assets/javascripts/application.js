d49c763bfb4a14f7f9215f121bfd44ca22e005e6
Maxime Boue <abriko@yahoo.fr>
2017-09-24 17:30:04 +0200

    Add checkpoint front and js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 //= require_tree .
 
 $(document).ready(function() {
@@ -23,6 +25,25 @@ $(document).ready(function() {
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
   });
+
+    $('#student').hide();
+    $('#company').hide();
+  });
+
+  $('#go-student').on('click', function(event){
+    $('#student').show();
+  })
+  $('#go-company').on('click', function(event){
+    $('#company').show();
+  })
+
+  $('#trigger-recapitulatif').on('click', function(event){
+    $('#prenom').html($('#user_first_name').val());
+    $('#nom').html($('#user_last_name').val());
+    $('#adresse').html($('#user_address').val());
+    $('#telephone').html($('#user_phone').val());
+    $('#email').html($('#user_email').val());
+    $('#classe').html($('#user_level').val());
 
   $('.attachinary-input').attachinary();
 });
