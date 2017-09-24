e11a74ea82bf4c71cbdadbda375525bad40db44c
jules maregiano <jules.maregiano@gmail.com>
2017-09-24 16:18:44 +0200

    lalala

--- app/controllers/hirings_controller.rb
+++ app/controllers/hirings_controller.rb
@@ -10,7 +10,16 @@ class HiringsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@hirings_users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-      # marker.json({ address: user.address })
+      marker.infowindow user.company
+      marker.picture({
+        :url     => if user.id > 30
+                      "https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/118ff475a341/assets/img/logos/favicon.ico"
+                    else
+                      "https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-32.png"
+                    end,
+        :width   => 32,
+        :height  => 32
+        })
     end
   end
 
