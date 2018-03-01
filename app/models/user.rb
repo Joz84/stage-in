397eaee762f6724068abf4b6e8446b7b859e22e4
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-03-01 14:56:24 +0100

    fix map

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,8 @@ class User < ApplicationRecord
   has_many :company_hirings, foreign_key: :company_id, class_name: "Hiring"
   has_many :student_hirings, foreign_key: :student_id, class_name: "StudentHiring"
 
-  # geocoded_by :address
-  # after_validation :geocode, if: :address_changed?
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   def full_name
     "#{first_name} #{last_name}"
@@ -28,4 +28,44 @@ class User < ApplicationRecord
   def marker_color
     rand[]
   end
+
+  def self.companies
+    where(role: "company")
+  end
+
+  def self.with_lng_lat
+    where
+    .not(latitude: nil, longitude: nil)
+  end
+
+  def gmap_hash(users)
+    Gmaps4rails.build_markers(users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow user.company
+      marker.picture({
+        url: user.marker_picture(self),
+        width: 32,
+        height: 32,
+        })
+    end
+  end
+
+  def marker_picture(current_user)
+    host = "http://res.cloudinary.com/zanzibar/image/upload/"
+    if self.distance_to(current_user) < 5
+      host + "v1506266970/GREEN_hkaart.png"
+    elsif self.distance_to(current_user) < 10
+      host + "v1506266970/ORANGE_fmuvmg.png"
+    else
+      host + "v1506266970/RED_onahwf.png"
+    end
+  end
+
+
+
+
+
+
+
 end
