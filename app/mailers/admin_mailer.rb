8675180753719b8d5d36de54ddcc02d15c0b8822
Claudine Pradal <claudinecartailler@gmail.com>
2018-10-01 12:21:05 +0200

    add mailer to admin with list of companies

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,7 @@ class AdminMailer < ApplicationMailer
 
   def new_company(user)
     @user = user  # Instance variable => available in view
+    @companies = User.companies
     mail(to: "y.couvant@cc-paysriberacois.fr", subject: ">> Stage-In - Inscription d'une nouvelle entreprise")
   end
 end
