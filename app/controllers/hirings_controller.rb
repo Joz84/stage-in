670f39327f1111c2c25206022d6bbc4f140f8e04
Benoit Calin <benoit.calin@gmail.com>
2019-06-21 14:12:47 +0200

    Fixed geocoding bug

--- app/controllers/hirings_controller.rb
+++ app/controllers/hirings_controller.rb
@@ -8,20 +8,22 @@ class HiringsController < ApplicationController
     rescue
       @hirings = { 3 => Hiring.college_filter(current_user) }
     end
-    # @companies = User.where(role: "company")
-    #                  .where
-    #                  .not(latitude: nil, longitude: nil)
-    #                  .limit(10)
-    # @hash = Gmaps4rails.build_markers(@companies) do |user, marker|
-    #   marker.lat user.latitude
-    #   marker.lng user.longitude
-    #   marker.infowindow user.company
-    #   marker.picture({
-    #     url: "http://res.cloudinary.com/zanzibar/image/upload/v1506266970/RED_onahwf.png",
-    #     width: 32,
-    #     height: 32,
-    #     })
-    # end
+
+    @companies = User.where(role: "company")
+                     .where
+                     .not(latitude: nil, longitude: nil)
+                     .limit(10)
+    @hash = Gmaps4rails.build_markers(@companies) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow user.company
+      marker.picture({
+        url: "http://res.cloudinary.com/zanzibar/image/upload/v1506266970/RED_onahwf.png",
+        width: 32,
+        height: 32,
+        })
+    end
+
     @student_hirings = current_user.student_hirings.order(:state).where.not(state: 0)
     @companies = User.companies
     @hash = current_user.gmap_hash(User.companies.with_lng_lat.limit(10))
