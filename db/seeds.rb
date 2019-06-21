9e2fd27f1b4f6b0bbc3b71cc6d632bbed189af49
Benoit Calin <benoit.calin@gmail.com>
2019-06-21 18:59:37 +0200

    Implement csv for the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Checkpoint.destroy_all
 
 puts "Seeding User Colleges"
 
-c1 = User.create(
+c1 = User.create!(
   college_name: "Collège Arnault Daniel",
   zipcode: "24600",
   address: "Rue Couleau",
@@ -24,7 +24,7 @@ c1 = User.create(
   email:"arnault.daniel@averifier.com",
   password: "123456",
   role: 'college')
-c2 = User.create(
+c2 = User.create!(
   college_name: "Collège Michel Debet",
   zipcode: "24350",
   address: "Route du Treuil",
@@ -32,7 +32,7 @@ c2 = User.create(
   email: "ce.0240073z@ac-bordeaux.fr",
   password: "123456",
   role: 'college')
-c3 = User.create(
+c3 = User.create!(
   college_name: "MFR du Ribéracois",
   zipcode: "24600",
   address: "Le Bourg",
@@ -45,7 +45,7 @@ puts "Colleges created"
 
 puts "Seeding User Student"
 
-User.create(
+User.create!(
   email: "jules@gmail.com",
   num: "30",
   address: "Rue Couleau",
@@ -69,47 +69,47 @@ puts "Seeding Skills"
 # SKILLS = SECTEUR D'ACTIVITE
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578053/SA_artisanat_mvmrvr.jpg"
-Skill.create(name: "Artisanat, Métiers du Batiment")
+Skill.create!(name: "Artisanat, Métiers du Batiment")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578054/SA_agriculture_b8up9i.jpg"
-Skill.create(name: "Agriculture, Agroalimentaire")
+Skill.create!(name: "Agriculture, Agroalimentaire")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578053/SA_assurance_ouojh5.jpg"
-Skill.create(name: "Secrétariat, Comptabilité, Commerce")
+Skill.create!(name: "Secrétariat, Comptabilité, Commerce")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578054/SA_commerce_s67fbz.jpg"
-Skill.create(name: "Métiers de bouche, Hôtellerie, Restauration")
+Skill.create!(name: "Métiers de bouche, Hôtellerie, Restauration")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578053/SA_industry_abd7bg.jpg"
-Skill.create(name: "Métiers de l’industrie et de la maintenance")
+Skill.create!(name: "Métiers de l’industrie et de la maintenance")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578053/SA_internet_ojzgiw.jpg"
-Skill.create(name: "Métiers du numérique")
+Skill.create!(name: "Métiers du numérique")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578053/SA_research_cns4mx.jpg"
-Skill.create(name: "Métiers du Social, de la Santé, Service à la personne")
+Skill.create!(name: "Métiers du Social, de la Santé, Service à la personne")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "http://res.cloudinary.com/stage-in/image/upload/v1519578054/SAtourisme_d6qtkw.jpg"
-Skill.create(name: "Tourisme, Culture, Sport")
+Skill.create!(name: "Tourisme, Culture, Sport")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 
 url = "https://images.pexels.com/photos/1267338/pexels-photo-1267338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-Skill.create(name: "Transport, logistique")
+Skill.create!(name: "Transport, logistique")
 Skill.last.photo_url = url
 puts "Création de la Skill #{Skill.last.name}"
 puts "Seeding internship..."
@@ -125,57 +125,55 @@ is = Internship.new(
   comment: "Stage de 3ème 2018-2019",
   college: c1
   )
-is.save
+is.save!
 is = Internship.new(
   starts_at: Date.new(2018, 12, 17),
   ends_at: Date.new(2018, 12, 21),
   comment: "Stage de 3ème 2018-2019",
   college: c2
   )
-is.save
+is.save!
 is = Internship.new(
   starts_at: Date.new(2019, 01, 14),
   ends_at: Date.new(2019, 01, 25),
   comment: "Stage de 3ème 2018-2019",
   college: c3
   )
-is.save
+is.save!
 
 puts "Internships created"
 
-# UNCOMMENT WHEN CSV FILE IMPLEMENTED
+puts "Seeding User Companies & Hirings"
 
-# puts "Seeding User Companies & Hirings"
+csv_options = { col_sep: ';', quote_char: "|", headers: :first_row }
+csv_filepath = File.read(Rails.root.join('db', 'csv', 'list_companies.csv'))
+csv_companies = CSV.parse(csv_filepath, csv_options)
+companies = []
 
-# csv_options = { col_sep: ';', quote_char: "|", headers: :first_row }
-# csv_filepath = File.read(Rails.root.join('db', 'csv', 'list_companies.csv'))
-# csv_companies = CSV.parse(csv_filepath, csv_options)
-# companies = []
+csv_companies.each do |row|
+  skill = Skill.find_by_name(row['skill'])
+  company = User.new(
+      company: row['company'],
+      email: row['email'],
+      password: "123soleil",
+      description: row['description'],
+      address: row['address'],
+      zipcode: row['zipcode'],
+      city: row['city'],
+      phone: row['phone'],
+      role: 'company'
+    )
+  company.skill = skill
+  company.save!
+  hiring = Hiring.new(
+    internship: Internship.all.sample,
+    company: company,
+    visible: true
+  )
+  hiring.save!
+end
 
-# csv_companies.each do |row|
-#   skill = Skill.find_by_name(row['skill'])
-#   company = User.new(
-#       company: row['company'],
-#       email: row['email'],
-#       password: "123soleil",
-#       description: row['description'],
-#       address: row['address'],
-#       zipcode: row['zipcode'],
-#       city: row['city'],
-#       phone: row['phone'],
-#       role: 'company'
-#     )
-#   company.skill = skill
-#   company.save!
-#   hiring = Hiring.new(
-#     internship: Internship.all.sample,
-#     company: company,
-#     visible: true
-#   )
-#   hiring.save!
-# end
-
-# puts "Companies & Hirings created"
+puts "Companies & Hirings created"
 
 puts "Seeding objectives"
 
@@ -184,54 +182,10 @@ checkpoints = [ "Je me renseigne sur l'entreprise.",
                 "Je prend rendez-vous avec l'entreprise."
               ]
 checkpoints.each_with_index do |title, i|
-  Checkpoint.create(title: title, order: i)
+  Checkpoint.create!(title: title, order: i)
 end
 
 puts "Objectives created"
-
-# DELETE BELOW WHEN CSV FILE IMPLEMENTED
-puts "Seeding companies and hirings"
-
-addresses = [
-  ['', 'Le Bourg', '24600', 'Allemans'],
-  ['10', 'Rue Alphonse Daudet', '24600', 'Ribérac'],
-  ['107', 'Rue de la Châtaigneraie', '24600', 'Ribérac'],
-  ['9', 'Quinquies r Couleau', '24600', 'RIBÉRAC'],
-  ['8', 'Place des Tilleuls', '24350', 'Tocane-Saint-Apre'],
-  ['', 'Le Bourg', '24600', 'Celles'],
-]
-
-30.times do
-  company = Faker::Company.name
-  email = company.parameterize + "@gmail.com"
-  address = addresses.sample
-  description = Faker::Company.catch_phrase
-  c = User.new
-  c.role = 0
-  c.company = company
-  c.description = description
-  c.skill = Skill.all.sample
-  c.email = email
-  c.password = "123456"
-  c.num = address[0]
-  c.address = address[1]
-  c.zipcode = address[2]
-  c.city = address[3]
-  c.phone = "0" + rand(100_000_000..999_999_999).to_s
-  puts c.errors.messages unless c.valid?
-  c.save
-  # create one hiring per company :
-  h = Hiring.new(
-    internship: Internship.all.sample,
-    company: c,
-    visible: true
-    )
-  h.save!
-end
-
-puts "Companies & hirings created"
-
-
 
 puts "The seed is done"
 
